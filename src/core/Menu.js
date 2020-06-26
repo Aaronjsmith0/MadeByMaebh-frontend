47172db1f7c0dfb47eacbec9099a05e1fc78ad21
Aaron Smith <aaron.smith@sgws.com>
2020-06-26 17:14:44 -0500

    made user and admin dashboard with private routes

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link, withRouter} from 'react-router-dom';
+import {signout, isAuthenticated} from '../auth';
 
 const isActive = (history, path) => {
     if(history.location.pathname === path) {
@@ -15,12 +16,36 @@ const Menu = ({history}) => (
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
             </li>
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
+            {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link className="nav-link" style={isActive(history, '/user/dashboard')} to="/user/dashboard">Dashboard</Link>
+                </li>
+            )}
+            {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                <li className="nav-item">
+                    <Link className="nav-link" style={isActive(history, '/admin/dashboard')} to="/admin/dashboard">Dashboard</Link>
+                </li>
+            )}
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
+                    </li>
+                </Fragment>
+            )}
+            {isAuthenticated() && (
+                <li className="nav-item">
+                <span className="nav-link" style={{cursor: 'pointer', color: '#ffffff'}} 
+                    onClick={() => signout(() => {
+                        history.push('/')
+                    })}
+                    >Signout
+                </span>
             </li>
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
-            </li>
+            )}
         </ul>
     </div>
 );
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import {Redirect} from 'react-router-dom';
 import Layout from '../core/Layout';
-import {signin} from '../auth';
+import {signin, authenticate, isAuthenticated} from '../auth';
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: '',
-        password: '',
+        email: 'maebh@gmail.com',
+        password: 'pass1234',
         error: '',
         loading: false,
         redirectToReferrer: false,
     })
 
-    const {email, password, loading, error, redirectToReferrer} = values
+    const {email, password, loading, error, redirectToReferrer} = values;
+    const {user} = isAuthenticated()
 
     const handleChange = name => event => {
         setValues({ ...values, error: false, [name]: event.target.value })
@@ -26,9 +27,11 @@ const Signin = () => {
             if(data.error) {
                 setValues({...values, error: data.error, loading: false})
             } else {
-                setValues({
-                    ...values,
-                    redirectToReferrer: true
+                authenticate(data, () => {
+                    setValues({
+                        ...values,
+                        redirectToReferrer: true
+                    })
                 })
             }
         })
@@ -72,14 +75,21 @@ const Signin = () => {
 
     const redirectUser = () => {
         if(redirectToReferrer) {
+            if(user && user.role === 1) {
+                return <Redirect to='/admin/dashboard' />
+            } else {
+                return <Redirect to='/user/dashboard' />
+            }
+        }
+        if(isAuthenticated()) {
             return <Redirect to='/' />
         }
     }
 
     return (
         <Layout 
-            title="Signup" 
-            description="Signup to Node React Ecommerce App"
+            title="Signin" 
+            description="Signin to Node React Ecommerce App"
             className='container col-md-8 offset-md-2'
         >
         {showLoading()}
