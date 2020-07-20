570ab776aebb20348e032af21791baaf0cc21b6c
Aaron Smith <aaron.smith@sgws.com>
2020-07-20 16:41:48 -0500

    took off auto fill for usernaem and passwrod

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -5,8 +5,8 @@ import {signin, authenticate, isAuthenticated} from '../auth';
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: 'aaronjsmith0@gmail.com',
-        password: 'Bearscare1!',
+        email: '',
+        password: '',
         error: '',
         loading: false,
         redirectToReferrer: false,
